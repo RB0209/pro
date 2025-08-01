2564baa62b790e5735a5a9fe866a0d03e6276aa3
Rishikesh <kesm3052021@gmail.com>
2025-08-01 15:52:45 +0530

    Add files via upload

---
+++ src/data/timelineData.js
@@ -0,0 +1,368 @@
+export const timelineData = {
+  "2011": [
+    {
+      title: "Social Awareness through Local Public Events and Programs during Festivals",
+      image: "/images/2011_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2012": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2012_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Socio-cultural program for youth motivation",
+      image: "/images/2012_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2013": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2013_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Socio-cultural program for youth motivation",
+      image: "/images/2013_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Summer camps and winter camps",
+      image: "/images/2013_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Radio City 91.1 FM",
+      image: "/images/2013_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2014": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2014_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Summer camps and winter camps",
+      image: "/images/2014_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2015": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2015_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "P3 Pre Primary",
+      image: "/images/2015_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Summer camps and winter camps",
+      image: "/images/2015_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2016": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2016_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "P3 Pre Primary",
+      image: "/images/2016_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Youth motivation programs",
+      image: "/images/2016_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2017": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2017_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "FANACT",
+      image: "/images/2017_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "DANSEN",
+      image: "/images/2017_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "P3 Pre Primary",
+      image: "/images/2017_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2018": [
+    {
+      title: "National Flag Pride Rally",
+      image: "/images/2018_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "FANACT",
+      image: "/images/2018_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Legal Awareness Seminar",
+      image: "/images/2018_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "AABAA",
+      image: "/images/2018_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "P3 Pre Primary",
+      image: "/images/2018_5.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Digi Womania Seminar",
+      image: "/images/2018_6.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Notebooks distribution",
+      image: "/images/2018_7.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Radio City 91.1 FM",
+      image: "/images/2018_8.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2019": [
+    {
+      title: "National Flag Pride Campaign",
+      image: "/images/2019_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Legal Awareness Seminars",
+      image: "/images/2019_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "DIGI WOMANIA Seminar",
+      image: "/images/2019_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "P3 Pre Primary",
+      image: "/images/2019_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Kerala flood donation",
+      image: "/images/2019_5.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Medical camp",
+      image: "/images/2019_6.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2020": [
+    {
+      title: "National Flag Pride Campaign",
+      image: "/images/2020_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Food, Water, Mask & Sanitizer Distribution",
+      image: "/images/2020_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Medical camp",
+      image: "/images/2020_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Sanitary pad distribution",
+      image: "/images/2020_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2021": [
+    {
+      title: "National Flag Pride Campaign",
+      image: "/images/2021_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Food & Water Distribution, Masks, Sanitizers",
+      image: "/images/2021_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Medical Aid under Project ASHA",
+      image: "/images/2021_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Juhu beach cleaning",
+      image: "/images/2021_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2022": [
+    {
+      title: "National Flag Pride Campaign",
+      image: "/images/2022_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Food & Water Distribution",
+      image: "/images/2022_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Masks, Sanitizers, Stationary Kit Distribution",
+      image: "/images/2022_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Sanitary pad distribution",
+      image: "/images/2022_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2023": [
+    {
+      title: "Project ASHA",
+      image: "/images/2023_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Feed The Need",
+      image: "/images/2023_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Stationary kit distribution",
+      image: "/images/2023_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2024": [
+    {
+      title: "Project ASHA",
+      image: "/images/2024_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Feed The Need",
+      image: "/images/2024_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Stationary kit distribution",
+      image: "/images/2024_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Food distribution",
+      image: "/images/2024_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ],
+  "2025": [
+    {
+      title: "Project ASHA",
+      image: "/images/2025_1.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Feed The Need",
+      image: "/images/2025_2.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Stationary kit distribution",
+      image: "/images/2025_3.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    },
+    {
+      title: "Food distribution",
+      image: "/images/2025_4.jpg",
+      shortDesc: "Placeholder short description",
+      longDesc: "Placeholder long description"
+    }
+  ]
+};
