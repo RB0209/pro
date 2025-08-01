17effd01c435276c5424f7aaf6139e41b2ad687b
Rishikesh <kesm3052021@gmail.com>
2025-08-01 16:31:46 +0530

    Update timelineData.js

--- src/data/timelineData.js
+++ src/data/timelineData.js
@@ -2,7 +2,7 @@ export const timelineData = {
   "2011": [
     {
       title: "Social Awareness through Local Public Events and Programs during Festivals",
-      image: "/Public/Image/1.png",
+      image: "Public/Image/1.png",
       shortDesc: "Placeholder short description",
       longDesc: "Placeholder long description"
     }
